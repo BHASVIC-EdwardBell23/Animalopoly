1cfb4aba50b0774ef5cb9907ff72dcea0111dd7c
BHASVIC-EthenDoherty23 <145350377+BHASVIC-EthenDoherty23@users.noreply.github.com>
2024-03-09 16:49:23 +0000

    updated todo list

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,8 @@ public class Main {
         //
         new Game(); //Starts game
 
-        /*List of stuff needed to do
-
-        Implement GUI: --> up to you
+        /*
+ todo       Implement GUI: --> up to you
              Board Display
              Dice Visual
              Card Display
@@ -23,7 +22,7 @@ public class Main {
              Pieces Move
 
 
-         GUI Logic:
+todo         GUI Logic:
             player can input amount of players -> PlayerManager ->constructor
             player can input if they want to replay -> up to you
             player can input name -> Player -> constructor
@@ -31,12 +30,12 @@ public class Main {
             click to buy/upgrade/downgrade/mortgage a property -> up to you
 
 
-        Logic:
+ todo       Logic:
             Bankruptcy -> validates Bankruptcy at the end of turn -> option to downgrade/mortgage properties
             Mortgaging Properties -> Mortgage gives player 1/2 buy value <- can do at start of turn if wanted but forced to sell if money <0
             Selling Upgrades -> player gets half of upgrade  price <- can do at start of turn but forced to sell if money < 0
             buying property -> only upon landing on property
-            pay rent -> upon landing no property 
+            pay rent -> upon landing no property
         */
     }
 }
