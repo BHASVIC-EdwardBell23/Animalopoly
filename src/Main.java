fefabb3ff99a7dc19df371e6d0a68226d9a9195e
elz03 <145988107+elz03@users.noreply.github.com>
2024-03-18 13:00:28 +0000

    c

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,8 @@ public class Main {
         new Game();
 
         /*
- todo       Implement GUI: --> up to you
-             Board Display
-
-
 
  todo        Property Display -- indicate Mortgaged or not
-             Turn Announcement
              Pieces Visually
              Players Select Unique Pieces (different colours/shapes?)
              Money Visual
@@ -30,12 +25,6 @@ todo       GUI Logic:
             player can click to Roll, display a certain property, mortgage a property if owned -> up to you
             click to buy/upgrade/downgrade/mortgage a property -> up to you
 
-
- todo       Logic:
-            Mortgaging Properties -> Mortgage gives player 1/2 buy value <- can do at start of turn if wanted but forced to sell if money <0
-            Selling Upgrades -> player gets half of upgrade  price <- can do at start of turn but forced to sell if money < 0
-            buying property -> only upon landing on property
-            pay rent -> upon landing no property
         */
     }
 }
